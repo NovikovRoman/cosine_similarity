01a0cae3601b8af3b31b7d4a61f4b755e46e5299
Novikov Roman <service.for.roman@gmail.com>
2023-01-25 13:02:30 +0300

    refact

--- cosine.go
+++ cosine.go
@@ -14,24 +14,29 @@ func Cosine(a []float64, b []float64) (cosine float64, err error) {
 	} else {
 		count = length_b
 	}
+
 	sumA := 0.0
 	s1 := 0.0
 	s2 := 0.0
 	for k := 0; k < count; k++ {
 		if k >= length_a {
-			s2 += math.Pow(b[k], 2)
+			s2 += b[k] * b[k]
 			continue
 		}
+
 		if k >= length_b {
-			s1 += math.Pow(a[k], 2)
+			s1 += a[k] * a[k]
 			continue
 		}
+
 		sumA += a[k] * b[k]
-		s1 += math.Pow(a[k], 2)
-		s2 += math.Pow(b[k], 2)
+		s1 += a[k] * a[k]
+		s2 += b[k] * b[k]
 	}
+
 	if s1 == 0 || s2 == 0 {
 		return 0.0, errors.New("Vectors should not be null (all zeros)")
 	}
+
 	return sumA / (math.Sqrt(s1) * math.Sqrt(s2)), nil
 }
